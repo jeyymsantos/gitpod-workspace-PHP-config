885257febacabc7f42ffd399c88ab4cd5975848d
Jeyym Santos <santosjl@students.nu-baliwag.edu.ph>
2022-04-01 14:36:11 +0000

    added select form for coutnry

--- laravel/resources/views/contactForm.blade.php
+++ laravel/resources/views/contactForm.blade.php
@@ -33,6 +33,27 @@
                 </div>
             </div>
             <input type="Address" class="form-control mb-2" name="address" placeholder="Address">
+
+            <div class="mb-2">
+              <label for="selectMenu" class="form-label">Country</label>
+              <select id="selectMenu" class="form-select" aria-label="Default select example">
+                  <option selected>- - Select Country - -</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+              </select>
+            </div>
+
+            <div class="mb-2">
+              <label for="selectMenu" class="form-label">State</label>
+              <select id="selectMenu" class="form-select" aria-label="Default select example">
+                  <option selected>- - Select State - -</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+              </select>
+            </div>
+
             <div class="d-flex justify-content-end">
                 <button type="submit" name="submit " class="btn btn-primary">Submit</button>
             </div>        
